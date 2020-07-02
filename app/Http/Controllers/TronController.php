82561b383964f9511a7eab2f32eb2be726e7a705
Filip Czapliński <filuuuus@gmail.com>
2020-07-02 22:28:36 +0200

    Create TronController

---
+++ app/Http/Controllers/TronController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TronController extends Controller
+{
+    public function createWallet()
+    {
+
+    }
+
+    public function getBallanceOfWallet()
+    {
+        
+    }
+}
